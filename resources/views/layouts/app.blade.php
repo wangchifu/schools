42045a4eb54e7e2f9b52af0bef2522a9c156c814
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-09-18 11:25:32 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>@yield('title')-彰化學校基本資料平台</title>
+        <title>@yield('title')-{{ env('APP_NAME') }}</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="{{ asset('assets/school.png') }}" />
         <!-- Bootstrap icons-->
